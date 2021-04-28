1dbd13f62a94ab6767eaec8fc89874aa3ae9c6c2
sdp256 <steevnpairy@gmail.com>
2021-04-28 18:16:31 -0500

    finished exercise

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,72 +6,81 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(1, 1, 1, 3)]
+        [InlineData(20, 40, 60, 120)]
+        [InlineData(-100, 100, 1000, 1000)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.Add(num1, num2, num3);
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]
-        public void Subtract(int minuend, int subtrhend, int expected)
+        [InlineData(200, 100, 100)]
+        [InlineData(69, 69, 0)]
+        public void Subtract(int minuend, int subtrahend, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.Subtract(minuend, subtrahend);
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(2, 2, 4)]
+        [InlineData(-5, 30, -150)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.Multiply(num1, num2);
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(10, 2, 5)]
+        [InlineData(100, 10, 10)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            var actual = test.Divide(num1, num2);
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
-        [Fact]
-        public void YourMethodName()
-        {
-            //Arrange
+        //[Fact]
+        //public void SayHello()
+        //{
+        //    //Arrange
+        //    var expected = "Hello";
+        //    //Act
+        //    var actual = SayHello.
+        //    //Assert
+        //    Assert.Equal(expected, actual);
+        //}
 
-            //Act
+        //[Fact]
+        //public void YourMethodName2()
+        //{
+        //    //Arrange
 
-            //Assert
+        //    //Act
 
-        }
-
-        [Fact]
-        public void YourMethodName2()
-        {
-            //Arrange
-
-            //Act
-
-            //Assert
-        }
+        //    //Assert
+        //}
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -4,23 +4,51 @@ namespace UnitTestingExercise
     public class UnitTestMethods
     {
         // Make sure your references are set properly
-            // Righ-click on Dependencies in your .Tests project and set your reference
+            // Right-click on Dependencies in your .Tests project and set your reference
 
 
 
         // Create an Add method that passes 3 integers
 
+        public int Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+
         // Create a Subtract method that passes 2 integers
             // Keep track of which number is getting passed as minuend and subtrahend
+        public int Subtract(int minuend, int subtrahend)
+        {
+            return minuend - subtrahend;
+        }
 
         // Create a Multiply method that passes 2 integers
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
 
         // Create a Divide method that passes 2 integers
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
 
 
 
-        // Create 2 methods that will utilize the [Fact] tests you wrote
+        //Create 2 methods that will utilize the[Fact] tests you wrote
+        //public string SayHello()
+        //{
+        //    return "Hello";
+        //}
 
-        
+        //public string SayGoodBye()
+        //{
+        //    return "GoodBye";
+        //}
+
+
+
     }
 }
